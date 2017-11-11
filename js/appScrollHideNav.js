1692fc6bf7bda09fa5f32428ad4787d7a7a04cdd
DeadShot90 <ajanth160899@gmail.com>
2017-11-11 20:39:14 +0100

    Added all pages with basic setup(topnav, breadcrumb menu, footer...)

---
+++ js/appScrollHideNav.js
@@ -0,0 +1,17 @@
+// Top bar scroll to hide
+// NOTE: Add script that adds margin to content when bar is showing
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 400) {
+    var scrollTop = $window.scrollTop();
+    nav.toggleClass('hidden', scrollTop > prev);
+    prev = scrollTop;
+    //document.getElementById("Test-Content").style.marginTop = '0px';
+  } else {
+    //document.getElementById("Test-Content").style.marginTop = '80px';
+  }
+});
