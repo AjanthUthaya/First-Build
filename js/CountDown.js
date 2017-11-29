7a85cbcee7daea5d650a7648513d447e08bbeb98
DeadShot90 <ajanth160899@gmail.com>
2017-11-29 17:29:38 +0100

    Fixed some small issues with coundown

--- js/CountDown.js
+++ js/CountDown.js
@@ -12,19 +12,12 @@ function CountDownTimer(dt, id, Test) {
     var distance = end - now;
     var Secounds = 0;
 
-    if (distance < 0) {
 
-      document.getElementById(id).innerHTML = 'Nah';
-
-      return;
-    }
     var Days = Math.floor(distance / _day);
     var Hours = Math.floor((distance % _day) / _hour);
     var Minutes = Math.floor((distance % _hour) / _minute);
     var Secounds = Math.floor((distance % _minute) / _second);
 
-    $("#NextClassHours").css("display", "none");
-    $("#NextClassDays").css("display", "none");
 
     if (Days > 0) {
 
@@ -32,8 +25,18 @@ function CountDownTimer(dt, id, Test) {
 
     } else {
 
-      $("#NextClassHours").css("display", "flex");
-      $("#NextClassDays").css("display", "none");
+      if (distance < 0) {
+
+        document.getElementById(id).innerHTML = 'Nah';
+        $("#NextClassNo").css("display", "flex");
+        $("#NextClassMajor").css("display", "none");
+
+      } else {
+        $("#NextClassHours").css("display", "flex");
+        $("#NextClassMajor").css("display", "block");
+        $("#NextClassDays").css("display", "none");
+      }
+
 
     }
 
@@ -62,19 +65,13 @@ function CountDownTimer(dt, id, Test) {
   }, 1000); // replace every 1000 millisecond = 1 sec
 }
 
-var CountDownTo = $("#NextClassHours").attr("CountDownTo");
-/*console.log(CountDownTo);*/
+var CountDownTo = $("#NextClassMain").attr("CountDownTo");
+//Code to get hours, minutes and secounds left until dateTime
 CountDownTimer(CountDownTo, 'NextHHours', 'hh');
-
 CountDownTimer(CountDownTo, 'NextHMinutes', 'mm');
-
 CountDownTimer(CountDownTo, 'NextHSecounds', 'ss');
 
-
-var CountDownTo = $("#NextClassDays").attr("CountDownTo");
-/*console.log(CountDownTo);*/
+//Code to get days, hours and minutes left until dateTime
 CountDownTimer(CountDownTo, 'NextDDays', 'dd');
-
 CountDownTimer(CountDownTo, 'NextDHours', 'hh');
-
 CountDownTimer(CountDownTo, 'NextDMinutes', 'mm');
