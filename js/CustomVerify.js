0e8bd71beb1d7b85acb8239efbc95ddef215a7e9
DeadShot90 <ajanth160899@gmail.com>
2017-12-03 16:02:54 +0100

    New major page done and added custom verify message

---
+++ js/CustomVerify.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var elements = document.getElementsByTagName("INPUT");//Change this to the tag you want to target
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].oninvalid = function(e) {
+      e.target.setCustomValidity("");
+      if (!e.target.validity.valid) {
+        e.target.setCustomValidity("This field cannot be left blank");
+      }
+    };
+    elements[i].oninput = function(e) {
+      e.target.setCustomValidity("");
+    };
+  }
+})
+//HTML code to set custom message
+//required oninvalid="this.setCustomValidity('Put  here custom message')"
