9fab281604b93b2478ddd912c2e2b50d20415614
DeadShot90 <ajanth160899@gmail.com>
2017-12-06 09:27:40 +0100

    Done with making data change on click for Accordion, and set default value which will be used to get value from php and display.

--- js/ShowContent.js
+++ js/ShowContent.js
@@ -1,4 +1,22 @@
 var Major = window.location.hash.substr(1);
-$( document ).ready(function() {
-    $("#Major-" + Major).css("display", "block");
+var DefaultSubMajor = '#' + 'Intro';
+
+$(document).ready(function() {
+  var DisplayMajor = $("#Major-" + Major).css("display", "block");
+  var DisplaySubMajor = $(DefaultSubMajor).css("display", "block");
+});
+
+//Changes Content on click
+$(document).on('click', '.Accord-List li a', function() {
+  var filterName = $(this).data('id-name');
+
+  var Id = '#' + filterName;
+  var Class = '.Sub-Major-Content-Main';
+
+  if ($(Id).length) {
+    $(Class).css("display", "none");
+    $(Id).css("display", "block");
+  } else {
+
+  }
 });
