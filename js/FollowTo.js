660e0711bb46bd7cf95ee6f42c11e180345f51ce
DeadShot90 <ajanth160899@gmail.com>
2017-12-14 14:35:22 +0100

    Test page fixed but change to absolute at the end of div is in place, needs fine tuning

--- js/FollowTo.js
+++ js/FollowTo.js
@@ -1,21 +1,37 @@
+$IdDataList = '#DataList';
+$IdOptionPanel = '#OptionMain';
+$ClassInside = 'Inside';
+$ClassOutside = 'Outside';
+$ClassInsideDot = '.Inside';
+$ClassOutsideDot = '.Outside';
+$($IdOptionPanel).addClass($ClassInside);
+
+//Glitching beacuse offset returns double value
 $(document).scroll(function() {
-  var element = '.Date-Add';
-  //get high parent element
-  /*var height_element_parent = $("#DataList").parent().outerHeight();*/
-  var height_element = $("#DataList").height() + 40; //get high of elemenet
-  var Button_POS = $(element).offset();
 
-  /*Button_css('class', 'position', 'top', 'bottom'); //(class or id, positon value, top value, bottom value)*/
 
-  if (Button_POS.top > height_element) {
-    $(element).css('position', `absolute`);
-    $(element).css('top', height_element);
-    $(element).css('bottom', `inherit`);
-    console.log("outside" + "Button:" + Button_POS.top + " element:" + height_element);
+  //Get height of Calendar height element
+  $CalHeight = $($IdDataList).height();
+  //Get position of options panel
+  $OptPOS = $($IdOptionPanel).offset();
+
+  $WindowBottomPOS = $(window).height() + $(window).scrollTop();
+
+  //Get bottom position of window
+  /*console.log('Position:' + $WindowBottomPOS);*/
+
+  //Change between to classes depening on position of option panel
+  if ($CalHeight > $OptPOS.top || $CalHeight > $WindowBottomPOS) {
+
+    $($IdOptionPanel).addClass($ClassInside);
+    $($IdOptionPanel).removeClass($ClassOutside);
+
   } else {
-    $(element).css('position', `fixed`);
-    $(element).css('top', `inherit`);
-    $(element).css('bottom', `10px`);
-    console.log("inside" + "Button:" + Button_POS.top + " element:" + height_element);
+
+    $($IdOptionPanel).addClass($ClassOutside);
+    $($IdOptionPanel).removeClass($ClassInside);
+
   }
+
+
 });
