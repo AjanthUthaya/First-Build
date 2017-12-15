0779e36550cefab84cb15e9397e525966a6bf26c
DeadShot90 <ajanth160899@gmail.com>
2017-12-15 15:46:24 +0100

    Glitch bug on appTest page(js/FollowTo.js), and removed unecessary href="#"

--- js/FollowTo.js
+++ js/FollowTo.js
@@ -6,7 +6,7 @@ $ClassInsideDot = '.Inside';
 $ClassOutsideDot = '.Outside';
 $($IdOptionPanel).addClass($ClassInside);
 
-//Glitching beacuse offset returns double value
+//Glitching beacuse offset returns double value, glitch shows up on screen size under 900px
 $(document).scroll(function() {
 
 
@@ -21,7 +21,7 @@ $(document).scroll(function() {
   /*console.log('Position:' + $WindowBottomPOS);*/
 
   //Change between to classes depening on position of option panel
-  if ($CalHeight > $OptPOS.top || $CalHeight > $WindowBottomPOS) {
+  if ($CalHeight > $OptPOS.top || $CalHeight > $WindowBottomPOS - 110) {
 
     $($IdOptionPanel).addClass($ClassInside);
     $($IdOptionPanel).removeClass($ClassOutside);
