f445dde49e29e9bcdc39f824653ff7271b01af92
DeadShot90 <ajanth160899@gmail.com>
2018-03-07 17:30:13 +0100

    Done with loading data from DB to home.php

--- php/Calendar/HomeInit.php
+++ php/Calendar/HomeInit.php
@@ -6,16 +6,17 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 //events.php
-include($_SERVER['DOCUMENT_ROOT'] .'/php/Addon/Dhtmlx/scheduler_connector.php');//includes the file
+require($_SERVER['DOCUMENT_ROOT'] . '/php/Addon/Dhtmlx/scheduler_connector.php');//includes the file
 
 // MySQLi
-include($_SERVER['DOCUMENT_ROOT'] .'/php/Addon/Dhtmlx/db_mysqli.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/php/Addon/Dhtmlx/db_mysqli.php');
 
-$res = new mysqli("localhost", "root", "root", "first-build");
+require($_SERVER['DOCUMENT_ROOT'] . '/php/Partials/DB.php');
+
 $dbtype = "MySQLi";
 
-$calendar = new schedulerConnector($res, $dbtype);//connector initialization
-$calendar->render_table("lessons", "lesson_id", "start_date, end_date, title, sub, room, ava, ava_max");
+$calendar = new schedulerConnector($conn, $dbtype);//connector initialization
+$calendar->render_table("lessons", "lesson_id", "id, type, start_date, end_date, teacher_id, title, sub, room, color, vgs, ava, ava_max, details");
 
 
 // NOTE: scheduler.getEvents(); to get all loaded events
